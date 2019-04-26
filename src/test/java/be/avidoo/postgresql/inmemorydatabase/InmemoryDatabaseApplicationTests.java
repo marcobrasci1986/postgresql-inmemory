5b440cd16582a0ba00056ff5d1cbc9405ae1e51e
marcobrasci1986 <marco_brasci@hotmail.com>
2019-04-26 09:17:20 +0200

    init commit, working version

--- src/test/java/be/avidoo/postgresql/inmemorydatabase/InmemoryDatabaseApplicationTests.java
+++ src/test/java/be/avidoo/postgresql/inmemorydatabase/InmemoryDatabaseApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class InmemoryDatabaseApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
