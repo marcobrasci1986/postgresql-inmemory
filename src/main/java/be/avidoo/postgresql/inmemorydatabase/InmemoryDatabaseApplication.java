2485ed3d174d155cd9d21818a44214fd127558e0
marcobrasci1986 <marco_brasci@hotmail.com>
2019-04-26 08:33:05 +0200

    init commit

---
+++ src/main/java/be/avidoo/postgresql/inmemorydatabase/InmemoryDatabaseApplication.java
@@ -0,0 +1,13 @@
+package be.avidoo.postgresql.inmemorydatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmemoryDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmemoryDatabaseApplication.class, args);
+	}
+
+}
